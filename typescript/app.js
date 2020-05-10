5b56efd4a332c95c74510ca667915d610ddeb9d2
iurimega13 <iurimega13@gmail.com>
2020-05-10 17:28:59 -0300

    Inferencia de tipos

--- typescript/app.js
+++ typescript/app.js
@@ -3,3 +3,10 @@ var ola = function (nome, sobrenome) {
     console.log("Ola " + nome + ' ' + sobrenome);
 };
 ola('iuri', 'galdino');
+var mensagem = "Seja Bem Vindo!!";
+var temporadasTheBoys = 1;
+var estudandoAngular = true;
+var listaDeAnimes = ["SAO", "KB", "TOG"];
+var listaDeAnimes2 = ["SAO", "KB", "TOG"];
+var listaDeNumeros = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11, 1, 1];
+var listaDeNumeros2 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11, 1, 1];
--- typescript/app.ts
+++ typescript/app.ts
@@ -3,3 +3,14 @@ let ola = (nome: string, sobrenome: string) => {
 }
 
 ola('iuri', 'galdino')
+
+let mensagem: string = "Seja Bem Vindo!!"
+let temporadasTheBoys: number = 1
+let estudandoAngular: boolean = true
+
+let listaDeAnimes: Array<string> = ["SAO","KB","TOG"]
+let listaDeAnimes2: string[] = ["SAO","KB","TOG"]
+
+
+let listaDeNumeros: Array<number> = [1,1,1,1,1,1,1,1,1,1,1,11,1,1]
+let listaDeNumeros2: number[] = [1,1,1,1,1,1,1,1,1,1,1,11,1,1]
