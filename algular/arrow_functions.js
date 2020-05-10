1e72c07cfd8f16dbb8615f4c5d06c1e5a5bbc8b6
iurimega13 <iurimega13@gmail.com>
2020-05-10 15:21:43 -0300

    Adicionando arquivos do curso

---
+++ algular/arrow_functions.js
@@ -0,0 +1,6 @@
+/*var dobroDoValor = function (numero) {
+    return numero * 2
+}*/
+var dobroDoValor = numero => numero * 2
+
+console.log(dobroDoValor(7));
---
+++ algular/variavel.js
@@ -0,0 +1,6 @@
+var serie = 'friends'
+
+if (true) {
+    serie = 'Game of Trones'
+}
+console.log(serie);
